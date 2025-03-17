1c199ec4741ebd6a182b54afa031cd991348b5e1
MarwenOnGit <marwenbenahmed89@gmail.com>
2025-03-17 11:55:40 +0100

    initial logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react"
 import LoginSignup from "./Components/LoginSignup/LoginSignup.jsx";
+import Logo from "./Components/Logo/Logo.jsx"
 import './App.css';
+
 
 function App() {
   return (
     <div>
+    <Logo/>
      <LoginSignup/>
     </div>
   );
---
+++ src/Components/Logo/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import "./Logo.css"
+const Logo = () => {
+  return (
+    <div>
+      <div className="logo">
+        <img src={logo} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Logo
